76708c8dd612a0ad6dd204a8596f29d0b8f2acc4
william <wayleemh@gmail.com>
2023-10-25 05:09:07 -0400

    feat: add settlement relation

--- src/shared/Settlement.ts
+++ src/shared/Settlement.ts
@@ -1,1 +1,14 @@
-export default class Settlement {}
+import PlayerData from "./PlayerData";
+import Vertex from "./Vertex";
+
+export default class Settlement {
+	player: PlayerData;
+	vertex: Vertex;
+
+	constructor(player: PlayerData, vertex: Vertex) {
+		this.player = player;
+		this.vertex = vertex;
+	}
+
+	collectResource(): void {}
+}
---
+++ src/shared/Vertex.ts
@@ -0,0 +1,29 @@
+import City from "./City";
+import Edge from "./Edge";
+import PlayerData from "./PlayerData";
+import Settlement from "./Settlement";
+import VertexCoordinate from "./VertexCoordinate";
+
+export default class Vertex {
+	edges: Edge[];
+	building?: Settlement | City;
+	position: VertexCoordinate;
+
+	constructor(position: VertexCoordinate) {
+		this.position = position;
+		this.building = undefined;
+		this.edges = [];
+	}
+
+	getPosition(): VertexCoordinate {
+		return this.position;
+	}
+
+	buildSettlement(player: PlayerData): void {
+		if (this.building === undefined) {
+			this.building = new Settlement(player, this);
+			player.buildings.removeBuilding({ settlement: 1 });
+			player.buildings.settlements.push(this.building as Settlement);
+		}
+	}
+}
