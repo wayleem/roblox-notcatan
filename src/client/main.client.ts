0336d6457afefe00d3630e067e3a824bb58a5777
william <wayleemh@gmail.com>
2023-11-27 22:54:05 -0500

    add main loop tests and todo

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,10 +1,36 @@
 import { makeHello } from "shared/module";
 import { Players } from "@rbxts/services";
 import PlayerData from "shared/PlayerData";
+import { new_game } from "server/main.server";
 
 const test = new PlayerData(Players.LocalPlayer, "red");
-test.resources.update("wood", 4);
+test.getResourceHand().update("wood", 4);
 
-test.resources.update("wood", -2);
-print(test.resources.get("wood"));
+test.getResourceHand().update("wood", -2);
+print(test.getResourceHand().get("wood"));
+
+const vertices = new_game.getVertices();
+
+vertices.forEach((v) => {
+	const part = v.getPart() as Part;
+	const highlight = part.WaitForChild("Highlight") as Highlight;
+	const clickDetector = part.WaitForChild("ClickDetector") as ClickDetector;
+	spawn(() =>
+		test
+			.getPlayer()
+			.GetMouse()
+			.Move.Connect(() => highlightUI(clickDetector, highlight)),
+	);
+});
+
+function highlightUI(clickDetector: ClickDetector, highlight: Highlight) {
+	clickDetector.MouseHoverEnter.Connect(() => (highlight.Enabled = true));
+}
+
+/*
+ * Find a way to give both client and server access to the same Game Instance so
+ * server can generate the board while client handles mouse hover logic for local
+ * use spawn multithreading to check for hover when player mouse moves instead of while true
+ * need to add mouse leave function too
+ */
 print(makeHello("main.client.ts"));
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,19 @@
+import { ServerStorage } from "@rbxts/services";
+import { Board } from "shared/Board";
 import { makeHello } from "shared/module";
+// 1 = 24 vertices and 30 edges
+//
+export const new_game = new Board();
+
+new_game.generateHexBoard(2, 10);
+
+const vertices = new_game.getVertices();
+
+vertices.forEach((v) => {
+	const part = v.getPart() as Part;
+	const highlight = part.WaitForChild("Highlight") as Highlight;
+	const clickDetector = part.WaitForChild("ClickDetector") as ClickDetector;
+	spawn(() => print("loop"));
+});
 
 print(makeHello("main.server.ts"));
