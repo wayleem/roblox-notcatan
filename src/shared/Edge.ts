2b9d7db64f8be3dcafa552e6dfa618db34528bf0
william <wayleemh@gmail.com>
2023-11-27 22:44:22 -0500

    remove coordinate class and put into main class

--- src/shared/Edge.ts
+++ src/shared/Edge.ts
@@ -1,1 +1,44 @@
-export default class Edge {}
+import Road from "./Road";
+import Vertex from "./Vertex";
+
+export default class Edge {
+	private vertices: Vertex[] = [];
+	private cframe: CFrame;
+	private road: Road | undefined;
+	private part: Part | undefined;
+
+	constructor(edgeStart: Vertex, edgeEnd: Vertex) {
+		this.vertices.push(edgeStart);
+		this.vertices.push(edgeEnd);
+		this.cframe = CFrame.lookAt(edgeStart.getPosition().add(edgeEnd.getPosition()).div(2), edgeEnd.getPosition());
+		this.road = undefined;
+		this.part = undefined;
+
+		edgeStart.addEdge(this);
+		edgeEnd.addEdge(this);
+	}
+
+	getVertices(): Vertex[] {
+		return this.vertices;
+	}
+
+	getCFrame(): CFrame {
+		return this.cframe;
+	}
+
+	getRoad(): Road | undefined {
+		return this.road;
+	}
+
+	getPart(): Part | undefined {
+		return this.part;
+	}
+
+	buildRoad(): Road {
+		return new Road(this);
+	}
+
+	setPart(part: Part) {
+		this.part = part;
+	}
+}
--- src/shared/Vertex.ts
+++ src/shared/Vertex.ts
@@ -1,29 +1,66 @@
+import { Building } from "./Buildings";
 import City from "./City";
 import Edge from "./Edge";
 import PlayerData from "./PlayerData";
 import Settlement from "./Settlement";
-import VertexCoordinate from "./VertexCoordinate";
 
 export default class Vertex {
-	edges: Edge[];
-	building?: Settlement | City;
-	position: VertexCoordinate;
+	private position: Vector3;
+	private edges: Edge[];
+	private building?: Settlement | City;
+	private part: Part | undefined;
 
-	constructor(position: VertexCoordinate) {
+	constructor(position: Vector3) {
 		this.position = position;
-		this.building = undefined;
 		this.edges = [];
+		this.building = undefined;
+		this.part = undefined;
 	}
-
-	getPosition(): VertexCoordinate {
+	getPosition(): Vector3 {
 		return this.position;
 	}
 
-	buildSettlement(player: PlayerData): void {
-		if (this.building === undefined) {
-			this.building = new Settlement(player, this);
-			//		player.buildings.removeBuilding({ settlement: 1 });
-			player.buildings.settlements.push(this.building as Settlement);
+	getEdges(): Edge[] {
+		return this.edges;
+	}
+
+	getBuilding(): Settlement | City | undefined {
+		return this.building;
+	}
+
+	getPart(): Part | undefined {
+		return this.part;
+	}
+
+	addEdge(edge: Edge): Edge[] {
+		this.edges.push(edge);
+		return this.edges;
+	}
+
+	setPart(part: Part) {
+		this.part = part;
+	}
+
+	//build physical building
+	build(player: PlayerData, buildingType: Building): Settlement | City {
+		if (buildingType === "settlement") {
+			if (this.building) {
+				error("A building already exists on this vertex.");
+			}
+			const settlement = new Settlement(this);
+			this.building = settlement;
+			player.getBuildings().add(settlement);
+			return settlement;
+		} else if (buildingType === "city") {
+			if (!(this.building instanceof Settlement)) {
+				error("No settlement to upgrade to a city.");
+			}
+			const city = new City(this.building);
+			this.building = city;
+			player.getBuildings().add(city);
+			return city;
+		} else {
+			error("Invalid building type.");
 		}
 	}
 }
