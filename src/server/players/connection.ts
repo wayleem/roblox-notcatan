1cf3f9af54d4c7ebed14d650ec921ac652dbdc99
william <wayleemh@gmail.com>
2024-04-19 10:53:22 -0400

    update

--- src/server/players/connection.ts
+++ src/server/players/connection.ts
@@ -1,30 +1,38 @@
 import { Players } from "@rbxts/services";
-import { create, del, flush } from "shared/actions";
+import { create, del, flush, update } from "shared/actions";
 import { store } from "server/store";
 import { PlayerState } from "server/store/players_reducer";
 import { serialize_userid } from "shared/utils";
+import { Resource } from "shared/types";
+import Object from "@rbxts/object-utils";
 
 export function on_player_join(player: Player) {
-	const initPlayer: PlayerState = {
-		teamColor: "red", // placeholder
-		resources: { wheat: 0, sheep: 0, ore: 0, wood: 0, brick: 0 },
-		devCards: { knight: 0, year_of_plenty: 0, monopoly: 0, road_building: 0, point: 0 },
-		roads: [],
-		settlements: [],
-		cities: [],
-		numPlayedKnights: 0,
-		numVictoryPoints: 0,
-	};
+  const initPlayer: PlayerState = {
+    teamColor: "red", // placeholder
+    resources: { wheat: 0, sheep: 0, ore: 0, wood: 0, brick: 0 },
+    devCards: { knight: 0, year_of_plenty: 0, monopoly: 0, road_building: 0, point: 0 },
+    roads: [],
+    settlements: [],
+    cities: [],
+    numPlayedKnights: 0,
+    numVictoryPoints: 0,
+  };
 
-	const playerId = serialize_userid(player.UserId);
+  const playerId = serialize_userid(player.UserId);
 
-	store.dispatch(flush(playerId, store.getState().board.vertices, "vertex"));
-	store.dispatch(flush(playerId, store.getState().board.edges, "edge"));
-	store.dispatch(flush(playerId, store.getState().board.hexes, "hex"));
+  store.dispatch(flush(playerId, store.getState().board.vertices, "vertex"));
+  store.dispatch(flush(playerId, store.getState().board.edges, "edge"));
+  store.dispatch(flush(playerId, store.getState().board.hexes, "hex"));
 
-	store.dispatch(create<PlayerState>(serialize_userid(player.UserId), initPlayer, "player"));
+  store.dispatch(create<PlayerState>(playerId, initPlayer, "player"));
+  store.dispatch(update<PlayerState>(playerId, "resources", { wheat: }))
 }
 
 export function on_player_leave(player: Player) {
-	store.dispatch(del(serialize_userid(player.UserId), "player"));
+  store.dispatch(del(serialize_userid(player.UserId), "player"));
+}
+
+function update(key: keyof Resource, count: number): Resource {
+  old_resource[key] += count
+  return old_resource
 }
--- src/shared/actions.ts
+++ src/shared/actions.ts
@@ -6,11 +6,11 @@ type Action_Create<T> = { id: string; data: T; type: "CREATE"; target: string };
 type Action_Merge<T> = { id: string; data: Partial<T>; type: "MERGE"; target: string };
 // change one key
 type Action_Update<T> = {
-	id: string;
-	key: keyof T;
-	value: unknown;
-	type: "UPDATE_KEY";
-	target: string;
+  id: string;
+  key: keyof T;
+  value: unknown;
+  type: "UPDATE_KEY";
+  target: string;
 };
 // delete
 type Action_Del = { id: string; type: "DEL"; target: string };
@@ -20,46 +20,46 @@ type Action_Flush<T> = { id: string; state: ArrayT<T>; type: "PING"; target: str
 export type MyActions<T> = Action_Create<T> | Action_Merge<T> | Action_Update<T> | Action_Del | Action_Flush<T>;
 
 export function create<T>(id: string, data: T, target: string): Action_Create<T> {
-	return {
-		type: "CREATE",
-		id,
-		data,
-		target,
-	};
+  return {
+    type: "CREATE",
+    id,
+    data,
+    target,
+  };
 }
 
 export function merge<T>(id: string, data: Partial<T>, target: string): Action_Merge<T> {
-	return {
-		type: "MERGE",
-		id,
-		data,
-		target,
-	};
+  return {
+    type: "MERGE",
+    id,
+    data,
+    target,
+  };
 }
 
 export function update<T>(id: string, key: keyof T, value: unknown, target: string): Action_Update<T> {
-	return {
-		type: "UPDATE_KEY",
-		id,
-		key,
-		value,
-		target,
-	};
+  return {
+    type: "UPDATE_KEY",
+    id,
+    key,
+    value,
+    target,
+  };
 }
 
 export function del(id: string, target: string): Action_Del {
-	return {
-		type: "DEL",
-		id,
-		target,
-	};
+  return {
+    type: "DEL",
+    id,
+    target,
+  };
 }
 
 export function flush<T>(id: string, state: ArrayT<T>, target: string): Action_Flush<T> {
-	return {
-		type: "PING",
-		id,
-		state,
-		target,
-	};
+  return {
+    type: "PING",
+    id,
+    state,
+    target,
+  };
 }
