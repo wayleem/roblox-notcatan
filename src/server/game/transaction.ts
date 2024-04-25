19704a2edea67b9d32d45c8dab5497b551534fe3
william <wayleemh@gmail.com>
2024-04-24 21:36:57 -0400

    update transaction logic

--- src/server/game/transaction.ts
+++ src/server/game/transaction.ts
@@ -1,15 +1,65 @@
 import { PlayerState } from "server/store/players_reducer";
 import { update } from "shared/actions";
-import { serialize_userid, update_hand } from "shared/utils";
+import { merge_hand, multiply_payload, serialize_userid } from "shared/utils";
 import { store } from "server/store";
+import { Hex } from "shared/types";
+import { DeckState } from "server/store/deck_reducer";
+import Object from "@rbxts/object-utils";
 
-export function tap_resource(p: Player) {
-	const player = store.getState().players[serialize_userid(p.UserId)];
+export function tap_resource(hex: Hex) {
+  hex.vertices.forEach((v) => {
+    if (v.building) {
+      const player = store.getState().players[v.building.ownerId]
+      if (player) {
 
-	if (player) {
-		const resources = player.resources;
-		const updated = update_hand(resources, "wheat", 5);
+        const resources = { ...player.resources };
+        const multiplier = v.building.points;
+        const tap = multiply_payload(hex.resource, multiplier);
 
-		store.dispatch(update<PlayerState>(serialize_userid(p.UserId), "resources", updated, "player"));
-	}
+
+        /* add finite # of cards later
+        const remove = multiply_payload(tap, -1)
+
+        if (merge_hand())
+
+        store.dispatch(update<DeckState>("", "resources", merge_hand(resources, remove), "deck"))
+        */
+
+        store.dispatch(update<PlayerState>(serialize_userid(v.building.ownerId), "resources", merge_hand(resources, tap), "player"));
+      }
+    }
+  })
+}
+
+export function draw_devcard(player: Player) {
+  const playerId = serialize_userid(player.UserId)
+  const playerData = store.getState().players[playerId]
+  const deck = store.getState().deck
+
+  if (!playerData) return
+
+  const availableCards = Object.entries(deck.devCards).filter(([card, count]) => count > 0);
+
+  if (availableCards.size() === 0) {
+    print("No development cards left to draw.");
+    return
+  }
+
+  // Randomly select a card to draw
+  const randomIndex = math.floor(math.random() * availableCards.size());
+  const [selectedCard,] = availableCards[randomIndex];
+
+  // Update the deck to remove one instance of the drawn card
+  store.dispatch(update<DeckState>("", 'devCards', {
+    [selectedCard]: deck.devCards[selectedCard] - 1
+  }, "deck"
+  ))
+
+  store.dispatch(update<PlayerState>(serialize_userid(player.UserId), 'devCards', {
+    ...playerData.devCards,
+    [selectedCard]: (playerData.devCards[selectedCard] || 0) + 1
+  }, 'player'));
+
+  print(`Player drew a ${selectedCard} card.`);
+
 }
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -4,70 +4,88 @@ import Object from "@rbxts/object-utils";
 import { MyActions } from "./actions";
 
 export function serialize_userid(userId: number): string {
-	return `player:${userId}`;
+  return `player:${userId}`;
 }
 
 export function deserialize_userid(userId: string): number {
-	const parts = userId.split(":");
-	return tonumber(parts[1], 10) as number;
+  const parts = userId.split(":");
+  return tonumber(parts[1], 10) as number;
 }
 
 export function serialize_vertex(vector: Vector3): string {
-	const x = tostring(math.round(vector.X * 100) / 100);
-	const y = tostring(math.round(vector.Y * 100) / 100);
-	const z = tostring(math.round(vector.Z * 100) / 100);
-	return `vertex:(${x}:${y}:${z})`;
+  const x = tostring(math.round(vector.X * 100) / 100);
+  const y = tostring(math.round(vector.Y * 100) / 100);
+  const z = tostring(math.round(vector.Z * 100) / 100);
+  return `vertex:(${x}:${y}:${z})`;
 }
 
 export function serialize_hex(vector: Vector3): string {
-	const x = tostring(math.round(vector.X * 100) / 100);
-	const y = tostring(math.round(vector.Y * 100) / 100);
-	const z = tostring(math.round(vector.Z * 100) / 100);
-	return `hex:(${x}:${y}:${z})`;
+  const x = tostring(math.round(vector.X * 100) / 100);
+  const y = tostring(math.round(vector.Y * 100) / 100);
+  const z = tostring(math.round(vector.Z * 100) / 100);
+  return `hex:(${x}:${y}:${z})`;
 }
 
 export function serialize_edge(cframe: CFrame): string {
-	const pos = cframe.Position;
-	const x = tostring(math.round(pos.X * 100) / 100);
-	const y = tostring(math.round(pos.Y * 100) / 100);
-	const z = tostring(math.round(pos.Z * 100) / 100);
+  const pos = cframe.Position;
+  const x = tostring(math.round(pos.X * 100) / 100);
+  const y = tostring(math.round(pos.Y * 100) / 100);
+  const z = tostring(math.round(pos.Z * 100) / 100);
 
-	return `edge:(${x},${y},${z})`;
+  return `edge:(${x},${y},${z})`;
 }
 
 export function is_edge(obj: Edge | Vertex): obj is Edge {
-	return "vertices" in obj;
+  return "vertices" in obj;
 }
 
 export function is_vertex(obj: Edge | Vertex): obj is Vertex {
-	return "edges" in obj;
+  return "edges" in obj;
 }
 
 export function is_vector3_equal(v1: Vector3, v2: Vector3, tolerance: number = 1e-4): boolean {
-	return math.abs(v1.X - v2.X) < tolerance && math.abs(v1.Y - v2.Y) < tolerance && math.abs(v1.Z - v2.Z) < tolerance;
+  return math.abs(v1.X - v2.X) < tolerance && math.abs(v1.Y - v2.Y) < tolerance && math.abs(v1.Z - v2.Z) < tolerance;
 }
 
 export function vector_to_string(v: Vector3): string {
-	return `${v.X}:${v.Y}:${v.Z}`;
+  return `${v.X}:${v.Y}:${v.Z}`;
 }
 
 export function someT<T>(data: ArrayT<T>, predicate: (value: T) => boolean): boolean {
-	const values = Object.values(data);
+  const values = Object.values(data);
 
-	return values.some((value) => {
-		if (value !== undefined) {
-			return predicate(value);
-		}
-		return false;
-	});
+  return values.some((value) => {
+    if (value !== undefined) {
+      return predicate(value);
+    }
+    return false;
+  });
 }
 
 export function remote_update_client<T>(action: MyActions<T>) {
-	const remoteEvent = ReplicatedStorage.WaitForChild("UpdateClientEvent") as RemoteEvent;
-	remoteEvent.FireAllClients(action);
+  const remoteEvent = ReplicatedStorage.WaitForChild("UpdateClientEvent") as RemoteEvent;
+  remoteEvent.FireAllClients(action);
 }
 
-export function update_hand<T = Resource | DevCard>(data: T, key: keyof T, count: number): T {
-	(data[key] as number) += count;
-	return data;
+export function add_to_hand<T extends Record<string, number>>(data: T, key: keyof T, count: number): T {
+  const newData = { ...data };
+  (newData[key] as number) += count;
+  return newData;
+}
+
+export function multiply_payload<T extends Record<string, number>>(data: T, multiplier: number): T {
+  const result: Partial<T> = {};
+
+  for (const [key, value] of Object.entries(data)) {
+    (result[key as keyof T] as number) = value as number * multiplier;
+  }
+  return result as T;
+}
+
+export function merge_hand<T extends Record<string, number>>(data: T, payload: T): T {
+  const newData = { ...data }
+  for (const [key, amount] of Object.entries(payload)) {
+    add_to_hand(newData, key as keyof T, amount as number);
+  }
+  return newData;
 }
